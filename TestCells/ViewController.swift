f8c310e69012caad490de35b675f2e71afe848d6
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-04-05 13:24:38 +0300

    Initial Commit

---
+++ TestCells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCells
+//
+//  Created by Rostyslav Druzhchenko on 05.04.2020.
+//  Copyright © 2020 Rostyslav Druzhchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
